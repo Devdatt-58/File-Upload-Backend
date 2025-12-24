4aab0486fe091d7565345910897201ffd70efebb
Devdatt-58 <singhdevdatt828@gmail.com>
2025-12-24 23:51:37 +0530

    Add File model for uploads

---
+++ models/File.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const fileSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String
+    },
+    imageUrl:{
+        type:String
+    },
+    tags:{
+        type:String
+    }
+})
+
+module.exports = mongoose.model("File",fileSchema);
